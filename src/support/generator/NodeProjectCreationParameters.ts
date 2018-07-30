b8a0657aa730e050d8e9e0493a871c114b512199
Rod Johnson <rod@atomist.com>
2018-07-30 11:12:53 +1000

    Fix package id

--- src/support/generator/NodeProjectCreationParameters.ts
+++ src/support/generator/NodeProjectCreationParameters.ts
@@ -34,7 +34,6 @@ export interface NodeProjectCreationParameters extends SeedDrivenGeneratorParame
 
 /**
  * Corresponding parameter definitions
- * @type {{appName: {displayName: string; description: string; pattern: RegExp; validInput: string; minLength: number; maxLength: number; required: boolean; order: number}; version: {pattern?: RegExp; required?: boolean; description?: string; displayName?: string; validInput?: string; displayable?: boolean; maxLength?: number; minLength?: number; type?: ParameterType; order?: number; group?: Group; control?: "input" | "textarea"; defaultValue: string}; screenName: {type: DeclarationType; uri: string}}}
  */
 export const NodeProjectCreationParametersDefinition: ParametersObject = {
 
